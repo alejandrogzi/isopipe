0dd6cd6cb65e82cd652306f4ce89d9f6d48a71ac
alejandrogzi <alejandrxgzi@gmail.com>
2025-04-29 17:15:02 +0200

    feat: strip const to own mod

---
+++ isopipe/src/consts.rs
@@ -0,0 +1,28 @@
+// formats
+pub const BAM: &str = "bam";
+pub const SAM: &str = "sam";
+
+// config pub const keys
+pub const OUTPUT_DIR: &str = "output_dir";
+pub const PRIMERS: &str = "primers";
+pub const INPUT_DIR: &str = "input_dir";
+pub const MEMORY: &str = "memory";
+pub const TIME: &str = "time";
+pub const PREFIX: &str = "prefix";
+pub const CHUNK: &str = "chunk";
+pub const REPORT: &str = "report-file";
+
+// project-wide pub const
+pub const ISOPIPE: &str = "isopipe";
+pub const PBINDEX: &str = "pbindex";
+pub const ISOTOOLS: &str = "isotools";
+pub const OUTPUT: &str = "isopipe_run";
+
+// filenames
+pub const NF_RUNNER: &str = "execute_joblist.nf";
+
+// managers consts
+pub const ASSETS: &str = "assets";
+pub const SHORT_QUEUE: &str = "short_queue";
+pub const DEFAULT_MEMORY: &str = "default_memory";
+pub const DEFAULT_THREADS: &str = "default_threads";
