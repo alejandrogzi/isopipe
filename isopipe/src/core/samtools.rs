55257d5d6d663e3409947e07360ce34b88f7220c
alejandrogzi <alejandrxgzi@gmail.com>
2025-05-03 13:55:49 +0200

    feat: scan_groups -> avoid unnecessary merging

--- isopipe/src/core/samtools.rs
+++ isopipe/src/core/samtools.rs
@@ -4,6 +4,7 @@ use crate::{
     core::pbindex,
     executor::{job::Job, manager::ParallelExecutor},
 };
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 /// Merge BAM files in a directory using samtools
@@ -28,58 +29,37 @@ use std::path::PathBuf;
 /// samtools::merge(&input_dir, &mut executor, &config);
 /// ```
 pub fn merge(input_dir: &PathBuf, executor: &mut ParallelExecutor, config: &Config) {
-    const THREADS: u32 = 8;
+    const THREADS: u32 = 16;
     const MEMORY: u32 = 8;
 
     let mut jobs = Vec::new();
     let mut pbi = Vec::new();
+
     let package = config.get_custom_package(SAMTOOLS);
+    let groups = scan_groups(input_dir);
 
-    for entry in std::fs::read_dir(input_dir)
-        .expect("Failed to read assets directory")
-        .flatten()
-        .filter(|entry| {
-            entry
-                .path()
-                .extension()
-                .and_then(|ext| ext.to_str())
-                .map(|ext| ext.eq_ignore_ascii_case(BAM))
-                .unwrap_or(false)
-        })
-    {
-        // INFO: format of chunked bams: {prefix}.{name}.ccs.{chunk}.bam
-        // INFO: basename -> {prefix}.{name}.ccs
-        let chunk = PathBuf::from(
-            entry
-                .path()
-                .file_stem()
-                .expect(&format!("Failed to get file name for {:?}", entry.path())),
-        );
-        let basename = chunk
-            .file_stem()
-            .expect(&format!("Failed to get file name for {:?}", entry.path()))
-            .to_string_lossy();
+    for (group, bams) in groups {
+        if bams.len() > 1 {
+            let merged = input_dir.join(format!("{}.ccs.{}", group, MERGED_BAM));
 
-        // INFO: format of merged: {prefix}.{name}.ccs.merged.bam
-        let merged = input_dir.join(format!("{}.{}", basename, MERGED_BAM));
+            if !merged.exists() {
+                // INFO: format of wildcard: {prefix}.{name}.ccs.*.bam
+                let wildcard = input_dir.join(format!("{}*{}", group, BAM));
 
-        if !merged.exists() {
-            // INFO: format of wildcard: {prefix}.{name}.ccs.*.bam
-            let wildcard = input_dir.join(format!("{}.*.{}", basename, BAM));
+                // INFO: if merged file does not existe, we merge and delete the unmerged files
+                let cmd = format!(
+                    "samtools merge -@{} {} {} && rm {}",
+                    THREADS,
+                    merged.display(),
+                    wildcard.display(),
+                    wildcard.display(),
+                );
 
-            // INFO: if merged file does not existe, we merge and delete the unmerged files
-            let cmd = format!(
-                "samtools merge -@{} {} {} && rm {}",
-                THREADS,
-                wildcard.display(),
-                merged.display(),
-                wildcard.display(),
-            );
+                pbi.push(merged);
 
-            pbi.push(merged);
-
-            let job = Job::from(cmd);
-            jobs.push(job);
+                let job = Job::from(cmd);
+                jobs.push(job);
+            }
         }
     }
 
@@ -93,4 +73,54 @@ pub fn merge(input_dir: &PathBuf, executor: &mut ParallelExecutor, config: &Conf
     );
 
     pbindex::pbindex(pbi, config, executor, input_dir);
+}
+
+/// Scan groups in the input directory and return a HashMap of group
+/// names to their corresponding BAM files.
+///
+/// # Arguments
+/// * `input_dir` - The input directory to scan for BAM files.
+///
+/// # Returns
+/// A HashMap of group names to their corresponding BAM files.
+///
+/// # Examples
+/// ```
+/// let input_dir = PathBuf::from("/path/to/input");
+/// let groups = scan_groups(&input_dir);
+/// assert_eq!(groups.len(), 2);
+/// assert_eq!(groups["group1"].len(), 3);
+/// assert_eq!(groups["group2"].len(), 2);
+/// ```
+fn scan_groups(input_dir: &PathBuf) -> HashMap<String, Vec<PathBuf>> {
+    let mut groups: HashMap<String, Vec<PathBuf>> = HashMap::new();
+
+    for entry in std::fs::read_dir(input_dir)
+        .expect("Failed to read assets directory")
+        .flatten()
+        .filter(|entry| {
+            entry
+                .path()
+                .extension()
+                .and_then(|ext| ext.to_str())
+                .map(|ext| ext.eq_ignore_ascii_case(BAM))
+                .unwrap_or(false)
+        })
+    {
+        // INFO: basename -> {prefix}.{name}.{*}.bam -> {name}
+        let bam = entry.path();
+        let basename = bam
+            .to_string_lossy()
+            .split(".")
+            .nth(1)
+            .expect(&format!(
+                "ERROR: Failed to get basename from {}",
+                entry.path().display()
+            ))
+            .to_string();
+
+        groups.entry(basename).or_insert(Vec::new()).push(bam);
+    }
+
+    groups
 }
