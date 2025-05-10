be4b259ca85c079fca2092b9a3e5023356a8a18e
alejandrogzi <alejandrxgzi@gmail.com>
2025-05-10 13:48:02 +0200

    chore: channeling errors

--- isopipe/src/core/minimap.rs
+++ isopipe/src/core/minimap.rs
@@ -81,11 +81,14 @@ pub fn minimap2(
 /// assert_eq!(fasta, "path/to/genome.fa");
 /// ```
 fn twobit_to_fa(genome: String, step_output_dir: &PathBuf) -> String {
-    let mut twobit = TwoBitFile::open(&genome).expect("ERROR: Failed to open 2bit file");
+    let mut twobit =
+        TwoBitFile::open(&genome).expect(&format!("ERROR: Failed to open 2bit file -> {}", genome));
     let fasta = step_output_dir.join(GENOME_FA);
 
-    let mut writer =
-        BufWriter::new(File::create(&fasta).expect("ERROR: Failed to create FASTA file"));
+    let mut writer = BufWriter::new(File::create(&fasta).expect(&format!(
+        "ERROR: Failed to create FASTA file -> {}",
+        fasta.display()
+    )));
 
     let _ = convert::fasta::to_fasta(&mut twobit, &mut writer)
         .expect("ERROR: Failed to convert 2bit to FASTA");
