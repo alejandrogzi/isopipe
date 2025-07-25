6e66368e104e71ecc342fecbfbd94662efe2934c
alejandrogzi <alejandrxgzi@gmail.com>
2025-07-25 14:41:05 +0200

    BREAKING CHANGE: agg_fusions in orf step!

--- isopipe/src/core.rs
+++ isopipe/src/core.rs
@@ -74,6 +74,7 @@ pub fn run_step(
         }
         PipelineStep::Orf => {
             log::info!("INFO [STEP 8]: Pre-processing for orf started...");
+            isotools::agg_fusions(&input_dir, executor, config);
             orf::orf(step, config, &input_dir, &step_output_dir)
         }
         PipelineStep::Nmd => {
