fe8384284d5c17df4c1d968e47ed180fe8fd3d1f
alejandrogzi <alejandrxgzi@gmail.com>
2025-07-29 01:12:21 +0200

    chore: backup + extract deprecated

--- isopipe/src/core/orf.rs
+++ isopipe/src/core/orf.rs
@@ -41,6 +41,8 @@ pub fn orf(
     let mut jobs = Vec::new();
     let args = config.get_step_custom_fields(step, vec![GENOME, ORFIPY, ORF_MIN_LEN, DATABASE]);
 
+    let mode = ParallelMode::from_str(&config.get_step_custom_field(step, PARALLEL_MODE));
+
     let twobit = PathBuf::from(args[0].clone());
     let chunk_size = crate::numerical!(config.get_step_custom_field(step, CHUNK) => usize)
         .unwrap_or_else(|e| panic!("ERROR: could not convert chunk to num -> {e}!"));
@@ -51,57 +53,145 @@ pub fn orf(
     );
     __merge_toga(step_output_dir, config, step);
 
-    // INFO: looping through all fusion outputs? -> free + fakes + review [other color + tag]; fusions
-    for file in FUSION_FILES {
-        let bed = input_dir.join(file);
+    // INFO: looping through all [per-chr or merged -> depend on parallel_mode opt]
+    // INFO: each [subdir/main] should have -> free + fakes + review [other color + tag] and fusions
+    match mode {
+        ParallelMode::Chromosome => {
+            for entry in std::fs::read_dir(input_dir)
+                .unwrap_or_else(|e| panic!("ERROR: could read directory -> {:?}. {e}", input_dir))
+                .flatten()
+                .filter(|e| e.path().is_dir())
+            {
+                let subdir = entry.path();
 
-        if !bed.exists() || std::fs::metadata(&bed).unwrap().len() == 0 {
-            log::warn!("WARN: {} does not exist or its empty!", bed.display());
-            continue;
+                // INFO: expected: fusions.bed / fusions.free.bed
+                for bed in std::fs::read_dir(&subdir)
+                    .unwrap()
+                    .flatten()
+                    .filter(|e| e.path().is_file())
+                {
+                    process_bed(
+                        bed.path(),
+                        &twobit,
+                        step_output_dir,
+                        chunk_size,
+                        &args,
+                        &mut jobs,
+                    );
+                }
+            }
         }
-
-        let basename = file.replace(".bed", "");
-        let suffix = basename
-            .split(".")
-            .last()
-            .unwrap_or_else(|| panic!("ERROR: could not get suffix from file -> {}", file));
-
-        // INFO: inflection point -> chunking fusion files [2nd chunking step in the pipeline]
-        let paths = extract(&bed, &twobit, step_output_dir, chunk_size, suffix);
-
-        for (chunked_fa, chunked_bed) in paths {
-            let chunked_dir = &chunked_bed.parent().unwrap_or_else(|| {
-                panic!(
-                    "ERROR: could not get parent directory for chunked_bed -> {}",
-                    chunked_bed.display()
-                )
-            });
-
-            let blast = format!(
-                "{} blast -e {} --fasta {} --alignments {} --outdir {} --orf-min-len {} --db {}",
-                ORF_RELEASE,
-                args[1], // INFO: orfipy
-                &chunked_fa.display(),
-                &chunked_bed.display(),
-                chunked_dir.display(),
-                args[2], // INFO: orf_min_len,
-                args[3], // INFO: database
-            );
-
-            let tai = format!(
-                "{} tai --fasta {} --alignments {} --outdir {}",
-                ORF_RELEASE,
-                chunked_fa.display(),
-                chunked_bed.display(),
-                chunked_dir.display(),
-            );
-
-            jobs.push(Job::from(blast));
-            jobs.push(Job::from(tai));
+        ParallelMode::Genome => {
+            for file in FUSION_FILES.iter().take(2) {
+                let bed = input_dir.join(file);
+                process_bed(bed, &twobit, step_output_dir, chunk_size, &args, &mut jobs);
+            }
         }
     }
 
     return jobs;
+}
+
+/// Processes a BED file by extracting sequences, chunking them, and generating
+/// `blast` and `tai` jobs for ORF detection.
+///
+/// This function takes a BED file and a 2bit genome file, extracts sequences
+/// corresponding to the BED entries, and then splits these extracted sequences
+/// and their corresponding BED entries into smaller "chunks." For each chunk,
+/// it constructs two command-line jobs: one for running `orf blast` and another
+/// for `orf tai`. These jobs are then added to a provided list of `Job` objects.
+/// Empty or non-existent BED files are logged as warnings and skipped.
+///
+/// # Arguments
+///
+/// * `bed` - A `PathBuf` representing the path to the input BED file.
+/// * `twobit` - A `PathBuf` representing the path to the 2bit genome file
+///              used for sequence extraction.
+/// * `step_output_dir` - A `PathBuf` representing the base output directory
+///                       for the current pipeline step. Chunked files will be
+///                       placed within subdirectories of this path.
+/// * `chunk_size` - An `usize` specifying the desired number of entries per chunk.
+/// * `args` - A reference to a `Vec<String>` containing command-line arguments.
+///            Specifically, `args[1]` is expected to be the `orfipy` executable path,
+///            `args[2]` is the minimum ORF length, and `args[3]` is the BLAST database path.
+/// * `jobs` - A mutable reference to a `Vec<Job>` where the generated
+///            `blast` and `tai` jobs will be appended.
+///
+/// # Panics
+///
+/// This function will panic if:
+/// - It cannot extract the file stem from the `bed` path.
+/// - It cannot determine a suffix from the `bed` file name.
+/// - The `extract` function (responsible for chunking) fails.
+/// - It cannot determine the parent directory for a chunked BED file.
+///
+/// # Example
+///
+/// ```rust, no_run
+/// process_bed(
+///     input_bed_path.clone(),
+///     &twobit_path,
+///     &output_dir,
+///     chunk_size,
+///     &cli_args,
+///     &mut job_list,
+/// );
+/// ```
+fn process_bed(
+    bed: PathBuf,
+    twobit: &PathBuf,
+    step_output_dir: &PathBuf,
+    chunk_size: usize,
+    args: &Vec<String>,
+    jobs: &mut Vec<Job>,
+) {
+    if !bed.exists() || std::fs::metadata(&bed).unwrap().len() == 0 {
+        log::warn!("WARN: {} does not exist or its empty!", bed.display());
+        return;
+    }
+
+    let basename = bed
+        .file_stem()
+        .unwrap_or_else(|| panic!("ERROR: could not get file stem from -> {:?}", bed))
+        .to_string_lossy();
+    let suffix = basename
+        .split(".")
+        .last()
+        .unwrap_or_else(|| panic!("ERROR: could not get suffix from file -> {:?}", bed));
+
+    // INFO: inflection point -> chunking fusion files [2nd chunking step in the pipeline]
+    let paths = extract(&bed, &twobit, step_output_dir, chunk_size, suffix);
+
+    for (chunked_fa, chunked_bed) in paths {
+        let chunked_dir = &chunked_bed.parent().unwrap_or_else(|| {
+            panic!(
+                "ERROR: could not get parent directory for chunked_bed -> {}",
+                chunked_bed.display()
+            )
+        });
+
+        let blast = format!(
+            "{} blast -e {} --fasta {} --alignments {} --outdir {} --orf-min-len {} --db {}",
+            ORF_RELEASE,
+            args[1], // INFO: orfipy
+            &chunked_fa.display(),
+            &chunked_bed.display(),
+            chunked_dir.display(),
+            args[2], // INFO: orf_min_len,
+            args[3], // INFO: database
+        );
+
+        let tai = format!(
+            "{} tai --fasta {} --alignments {} --outdir {}",
+            ORF_RELEASE,
+            chunked_fa.display(),
+            chunked_bed.display(),
+            chunked_dir.display(),
+        );
+
+        jobs.push(Job::from(blast));
+        jobs.push(Job::from(tai));
+    }
 }
 
 /// Extract sequences for fusion-free predicted
@@ -121,13 +211,14 @@ pub fn orf(
 /// # Example
 ///
 /// ```rust
-/// /// let jobs = orf(
+/// let jobs = orf(
 ///     &step,
 ///     &config,
 ///     &input_dir,
 ///     &step_output_dir,
 /// );
 /// ```
+#[deprecated = "See ../../assets/extract for binary version!"]
 pub fn extract(
     reads: &PathBuf,
     twobit: &PathBuf,
