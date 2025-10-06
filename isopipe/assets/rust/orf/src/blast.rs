0ddd79341f5470b98bea86b2d1d8b09ea486802d
alejandrogzi <alejandrxgzi@gmail.com>
2025-10-07 00:28:37 +0200

    fix: missing / in env

--- isopipe/assets/rust/orf/src/blast.rs
+++ isopipe/assets/rust/orf/src/blast.rs
@@ -38,7 +38,7 @@ const HEADER_REGEX: &str = r"\[(\d+)-(\d+)\]\(([+-])\)";
 // pub const VENV: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tai/.venv/bin/activate");
 pub const VENV: &str = concat!(
     env!("CARGO_MANIFEST_DIR"),
-    "../../py/orfipy/.venv/bin/activate"
+    "/../../py/orfipy/.venv/bin/activate"
 );
 
 /// Runs a complete BLAST analysis pipeline, including ORF prediction, deduplication,
